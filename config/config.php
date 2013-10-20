02efb9831bae3f8a721df2e5d821f079c5b13af8
daneast915 <daneast915@Daniel-Easts-iMac.local>
2013-10-20 17:32:47 -0400

    Deploying to live server

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p2_calendarte_biz'); 
+	define('DB_NAME', 'calendar_p2_calendarte_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
